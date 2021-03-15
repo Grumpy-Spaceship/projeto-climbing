ced6d98a873fade3ec7f8d03cdc16f1a82d2afe6
Pedro Marangon <pedrommarangon@gmail.com>
2021-03-15 11:25:14 -0300

    Settings changed

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+// Maded by Pedro M Marangon
+using UnityEngine;
+
+namespace Game.Player
+{
+	public class Player : MonoBehaviour
+	{
+
+		// Awake is called before the first frame update
+		private void Awake()
+		{
+
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+
+		}
+
+	}
+}
