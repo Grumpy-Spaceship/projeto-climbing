52b88eed8a13dc34981ea225b8ff2c72b55fea1a
Pedro Marangon <pedrommarangon@gmail.com>
2021-05-24 13:06:34 -0300

    Revert "Now the level destroys itself after camera moves on"
    This reverts commit fae484aaca5b3cf619bc9e742f2766575c940617.

--- Assets/_ProjectAssets/Scripts/Level Generator/CreateLevelPiece.cs
+++ Assets/_ProjectAssets/Scripts/Level Generator/CreateLevelPiece.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class CreateLevelPiece : MonoBehaviour
 {
-	[SerializeField] private GameObject obj = null;
-	private LevelGenerator levelGenerator;
-	private bool destroyObject = false;
+
+    private LevelGenerator levelGenerator;
 
 	private void Awake() => levelGenerator = FindObjectOfType<LevelGenerator>();
 
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		if (!destroyObject)
-		{
-			levelGenerator?.Generate(transform.position);
-			destroyObject = true;
-			return;
-		}
-		
-		if(other.CompareTag("Destroy Level Piece"))
-			Destroy(obj);
+		levelGenerator?.Generate(transform.position);
+		Destroy(gameObject);
 	}
 
 }
